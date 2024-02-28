695632a4f1e22c6b90b5103cfc65d4781f89aebb
Fatemeh Sadat Hashemi Golpayegans <fatisati@gmail.com>
2024-02-28 07:56:50 +0100

    add interpretablity for prototypes

--- interpretation.py
+++ interpretation.py
@@ -1,14 +1,16 @@
 import torch
-from pathlib import Path
 import requests
 from autoencoder import *
 import json
-
+from pancras_data import PancrasDataset
+from torch.utils.data import DataLoader
+import pancras_train
+from prototype_classifier import ProtClassifier
 
 def calculate_mean_var_latent(model, data_loader):
     z_arr = []
-    for batch, adata in enumerate(data_loader):
-        z = model.encoder(adata.X)
+    for x, _ in data_loader:
+        z = model.encoder(x)
         z_arr.append(z)
     z_arr_tensor = torch.cat(z_arr, 0)
     var, mean = torch.var_mean(z_arr_tensor, dim=0)
@@ -35,28 +37,33 @@ def downstream(gene_names):
         json={
             "organism": "hsapiens",
             "sources": sources,
-            "query": genes,
+            "query": gene_names,
         },
     )
-    return r.json['result']
-
+    return r.json()['result']
+import utils, pancras_data
 if __name__ == "__main__":
 
-    device = get_device()
+    device = utils.get_device()
 
     # load data
     batch_size = 64
-    data_path = get_data_path()
-
     print("loading data")
-    data = sc.read_h5ad(data_path)
-    input_dim = data[0].shape[1]
-    data_loader = AnnLoader(data, batch_size=batch_size, use_cuda=device)
+    pancras_data = PancrasDataset(device)
+    data_loader = DataLoader(pancras_data, batch_size=batch_size, shuffle=False)
 
     # load model
-    model_path = get_model_path()
-    hidden_dim, latent_dim = 256, 128
-    model = VariationalAutoencoder(input_dim, hidden_dim, latent_dim)
+    # define model
+    num_prototypes, num_classes = 4, 14
+    input_dim, hidden_dim, latent_dims = 4000, 64, 8
+    model = ProtClassifier(
+        num_prototypes=num_prototypes,
+        num_classes=num_classes,
+        input_dim=input_dim,
+        hidden_dim=hidden_dim,
+        latent_dims=latent_dims,
+    )
+    model_path = pancras_train.get_model_path(num_prototypes)
     model.load_state_dict(torch.load(model_path)["model_state_dict"])
     model.to(device)
 
@@ -67,20 +74,25 @@ if __name__ == "__main__":
     # decode the mean latent vector and find mean cell
     mean_cell = model.decoder(mean)
 
-    # for each dimension of latent vector change it by 2 sigma of that dimension
-    # decode these vectors and calculate difference with mean cell
-    dim_cells = calculate_all_dimension_cells(mean, var, model)
-    diff_mean_cell = dim_cells - mean_cell
+    # # for each dimension of latent vector change it by 2 sigma of that dimension
+    # # decode these vectors and calculate difference with mean cell
+    # dim_cells = calculate_all_dimension_cells(mean, var, model)
+    # diff_mean_cell = dim_cells - mean_cell
+    
+    # decode all prototypes and compare them with mean cell
+    prot_cells = model.decoder(model.prototype_vectors)
+    prot_diff = prot_cells - mean_cell
 
     # find k mostly affected genes for each dim
-    k = 3
-    top_idx = torch.topk(diff_mean_cell, k).indices
-    tops_genes = [list(data.var.index[idx.cpu()]) for idx in top_idx]
+    k = 5
+    top_idx = torch.topk(prot_diff, k).indices
+    tops_genes = [list(pancras_data.adata.var.index[idx.cpu()]) for idx in top_idx]
 
     # do downstream analysis on mostly affected genes (go analysis - pathway analysis)
     res = {}
     for genes in tops_genes:
         res[str(genes)] = downstream(genes)
+        print(len(res[str(genes)]))
         
     with open('downstream.json', 'w') as f:
         json.dump(res, f)
