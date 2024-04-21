c3f4c40982e2acf8680ee86761004ada9c7cda1f
Fatemeh Sadat Hashemi Golpayegans <fatisati@gmail.com>
2024-04-21 07:18:47 +0200

    debug protoype barlow model loss

--- interpretable_ssl/models/prototype_barlow.py
+++ interpretable_ssl/models/prototype_barlow.py
@@ -34,7 +34,7 @@ class PrototypeBarlow(nn.Module):
         prot_loss2.calculate(x2, x_hat2, z2, self.prototype_model)
 
         batch_loss = prot_loss1 + prot_loss2
-        batch_loss.model_loss = barlow_loss
+        batch_loss.set_model_loss(barlow_loss)
         return batch_loss
 
 
@@ -71,34 +71,42 @@ class BarlowProjector(nn.Module):
         return loss
 
 
+def optimize_model(x1, x2, model, optimizer, overal_loss):
+    # 1. Forward pass
+    # 2. Calculate loss
+    batch_loss = model(x1, x2)
+    overal_loss += batch_loss
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Loss backward
+    batch_loss.loss.backward()
+
+    # torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0) # or some other value
+
+    # 5. Optimizer step
+    optimizer.step()
+    
 def train_step(model: PrototypeBarlow, data_loader, optimizer, device):
     model.to(device)
 
     overal_loss = PrototypeLoss()
 
     for x1, x2 in data_loader:
-        # 1. Forward pass
-        # 2. Calculate loss
-        x1, x2 = x1.to(device), x2.to(device)
-        batch_loss = model(x1, x2)
-        overal_loss += batch_loss
 
-        # 3. Optimizer zero grad
-        optimizer.zero_grad()
+        if data_loader.dataset.dataset.multiple_augment_cnt:
+            for x2_item in x2:
+                optimize_model(x1, x2_item, model, optimizer, overal_loss)
+        else:
+            optimize_model(x1, x2, model, optimizer, overal_loss)
 
-        # 4. Loss backward
-        batch_loss.loss.backward()
-
-        # torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0) # or some other value
-
-        # 5. Optimizer step
-        optimizer.step()
 
     overal_loss.normalize(len(data_loader))
     return overal_loss
 
 
-def test_step(data_loader, model, device):
+def test_step(model, data_loader, device):
 
     test_loss = PrototypeLoss()
     model.to(device)
